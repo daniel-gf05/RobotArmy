7a55769754a01caad89fc4df516861e1fdef773a
daniel-gf05 <danielfgayoso@gmail.com>
2025-05-16 17:14:07 +0200

    first_commit

---
+++ src/main/java/com/daniel/robot/RobotArmyApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotArmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotArmyApplication.class, args);
+	}
+
+}
